d9a96b257ba09c1b2e97a3e8cee8ec5bec122929
Brisk4t <31730769+Brisk4t@users.noreply.github.com>
2023-07-04 16:04:48 -0600

    Update ditts.py

--- ditts.py
+++ ditts.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 intents = discord.Intents.all()
 command_prefix = '//'
-TOKEN = ('MTA3OTg2MjIxNjkxMzg2Njc2Mg.G1gGlJ.NmasFyzo4kzny8vOHoz4GJ1GVB0eCdW45keMbM')
+TOKEN = (TOKEN)
 bot = commands.Bot(command_prefix=command_prefix, intents=intents)
 
 
