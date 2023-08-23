bd133201b3535b7fdf6625c42a2cc7c0994b44bb
Brisk4t <31730769+Brisk4t@users.noreply.github.com>
2023-08-23 10:19:43 -0600

    Update TTShandler.py

--- cogs/TTShandler.py
+++ cogs/TTShandler.py
@@ -47,7 +47,7 @@ class TextToSpeech():
         save(audio, "output.mp3")
 
         source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("output.mp3"))
-	source.volume = 1
+	    source.volume = 1
         voice_channel.play(source)
     
     
